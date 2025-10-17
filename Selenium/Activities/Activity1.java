0d66b145234c63ce95a5c6a2c734dbb6854f3829
maheshbansode57579 <maheshbansode57579@gmail.com>
2025-10-17 19:23:50 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity1.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity1 {
+    public static void main(String[] args) {
+        // Initialize the Firefox Driver
+        WebDriver driver = new ChromeDriver();
+
+        // Open the page
+        driver.get("https://training-support.net");
+
+        // Verify the page title
+        System.out.println("Page title is: " + driver.getTitle());
+        // Find the About Us link and click it
+        driver.findElement(By.linkText("About Us")).click();
+        // Print the page title of the About Us page
+        System.out.println("New page title is: " + driver.getTitle());
+
+        // Close the browser
+        driver.quit();
+    }
+}
