92a47ab5a2cf8c9bf988c35b010ff40f528e74f2
maheshbansode57579 <maheshbansode57579@gmail.com>
2025-11-17 19:36:34 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity5_DemoTwo.java
@@ -0,0 +1,11 @@
+package TestNGPackage;
+
+
+import org.testng.annotations.Test;
+
+public class Activity5_DemoTwo {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
